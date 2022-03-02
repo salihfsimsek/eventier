0c3b352e1fff263e940ff229abf88e1a4912befb
Salih Simsek <salihsimseek@gmail.com>
2022-03-02 23:13:02 +0300

    user model created

---
+++ v1/src/models/user-model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+    full_name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: false
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String,
+        required: true
+    },
+    profile_image: {
+        type: String,
+        required: true
+    }
+},{timestamps: true, versionKey: false})
+
+const UserModel = mongoose.model('user', UserSchema)
+
+module.exports = UserModel
