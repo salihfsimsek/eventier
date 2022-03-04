79a242e25adfe0c41d1b089e5668e9c9eac2f933
Salih Simsek <salihsimseek@gmail.com>
2022-03-04 16:21:05 +0300

    refresh and access tokens generated

--- v1/src/controllers/user-controllers.js
+++ v1/src/controllers/user-controllers.js
@@ -1,6 +1,6 @@
 const UserService = require('../services/user-service')
 
-const {passwordToHash} = require('../scripts/utils/helper')
+const {passwordToHash, generateAccessToken, generateRefreshToken} = require('../scripts/utils/helper')
 
 const create = async (req,res,next) => {
     req.body.password = passwordToHash(req.body.password)
@@ -17,15 +17,20 @@ const login = async (req, res, next) => {
     try{
         let findUser
         if(req.body.email){
-            findUser = await UserService.findOne({email: req.body.login, password: password})
+            findUser = await UserService.findOne({email: req.body.email, password: password})
             if(!findUser) return next({message: 'Email or password wrong', status: 401})
         }
         else if(req.body.username){
-            findUser = await UserService.findOne({username: req.body.login, password: password})
+            findUser = await UserService.findOne({username: req.body.username, password: password})
             if(!findUser) return next({message: 'Username or password wrong', status: 401})
         }
+        
+        let data = {
+            access_token: generateAccessToken({id: findUser._id, email: findUser.email}),
+            refresh_token: generateRefreshToken({id: findUser._id, email: findUser.email})
+        }
 
-        res.status(201).send(findUser)
+        res.status(201).send(data)
 
     }catch(err){
         next(err)
--- v1/src/scripts/utils/helper.js
+++ v1/src/scripts/utils/helper.js
@@ -1,7 +1,15 @@
 const CryptoJS = require('crypto-js')
-
+const jwt = require('jsonwebtoken')
 const passwordToHash = (password) => {
     return CryptoJS.HmacSHA256(password, CryptoJS.HmacSHA1(password, process.env.PASSWORD_HASH_KEY).toString()).toString()
 }
 
-module.exports = {passwordToHash}
+const generateAccessToken = (user) => {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY, {expiresIn: '1w'})
+}
+
+const generateRefreshToken = (user) => {
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET_KEY, {expiresIn: '2w'})
+}
+
+module.exports = {passwordToHash, generateAccessToken, generateRefreshToken}
