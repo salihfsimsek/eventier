5e1fb29c907865054672f3c1300f2ec544700e25
Salih Simsek <salihsimseek@gmail.com>
2022-03-08 23:51:33 +0300

    idChecker middleware created

---
+++ v1/src/middlewares/idChecker.js
@@ -0,0 +1,7 @@
+const idChecker = (field) => async (req, res, next) => {
+    if (!req.params[field || 'id']?.match(/^[0-9a-fA-F]{24}$/)) {
+        next({message: 'ID not valid', status: 400})
+        return;
+    }
+    next()
+}
